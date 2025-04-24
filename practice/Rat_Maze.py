e9b1f0f20a43bafb1637ac7937a73a863f653eff
jorted-runner <jorted.python@gmail.com>
2025-04-24 17:02:47 -0500

    Rat MAze

---
+++ practice/Rat_Maze.py
@@ -0,0 +1,25 @@
+"""
+Given a square grid of N x N with 0s and 1s where 1 represents an open cell and 0 a blocked one, find a path from the top-left cell to the bottom-right cell, moving only right or down.
+
+Input:
+The first line contains the integer N.
+The next N lines contain N integers (0 or 1) each.
+
+Output:
+Print "YES" and the N x N path matrix showing the route using 1s.
+If no path exists, print "NO".
+
+Constraints:
+1 <= N <= 10
+"""
+
+# Check Down
+def check_down(maze, x, y):
+    ...
+
+# Check Right
+def check_right(maze, x, y):
+    ...
+
+def solve_maze(maze, x=0, y=0):
+    ...
