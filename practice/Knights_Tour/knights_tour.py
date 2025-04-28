14b0bc2e98de7f18103a32fad70af3314f779234
jorted-runner <jorted.python@gmail.com>
2025-04-28 15:23:40 -0500

    Refactoring and starting on the new problem

---
+++ practice/Knights_Tour/knights_tour.py
@@ -0,0 +1,14 @@
+"""
+Knight's Tour
+Given an N x N chessboard, place a knight on the board such that it visits every square exactly once.
+
+Input:
+A single integer N.
+
+Output:
+Print "YES" followed by the board with move numbers from 1 to N*N.
+If a full tour is not possible, print "NO".
+
+Constraints:
+5 <= N <= 8
+"""
